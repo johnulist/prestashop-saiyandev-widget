b531ced1f90a9d4ee037e495b031d9dcd02ed606
jotraverso <jotraverso@gmail.com>
2013-04-22 20:22:58 +0200

    jcarrousell unpacked

--- saiyandev-prestashop/js/jcarousel.js
+++ saiyandev-prestashop/js/jcarousel.js
@@ -1,1 +1,129 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(6($){$.1g.1w=6(o){o=$.1f({r:n,x:n,N:n,17:q,J:n,L:1a,16:n,y:q,u:12,H:3,B:0,k:1,K:n,I:n},o||{});8 G.R(6(){p b=q,A=o.y?"15":"w",P=o.y?"t":"s";p c=$(G),9=$("9",c),E=$("10",9),W=E.Y(),v=o.H;7(o.u){9.1h(E.D(W-v-1+1).V()).1d(E.D(0,v).V());o.B+=v}p f=$("10",9),l=f.Y(),4=o.B;c.5("1c","H");f.5({U:"T",1b:o.y?"S":"w"});9.5({19:"0",18:"0",Q:"13","1v-1s-1r":"S","z-14":"1"});c.5({U:"T",Q:"13","z-14":"2",w:"1q"});p g=o.y?t(f):s(f);p h=g*l;p j=g*v;f.5({s:f.s(),t:f.t()});9.5(P,h+"C").5(A,-(4*g));c.5(P,j+"C");7(o.r)$(o.r).O(6(){8 m(4-o.k)});7(o.x)$(o.x).O(6(){8 m(4+o.k)});7(o.N)$.R(o.N,6(i,a){$(a).O(6(){8 m(o.u?o.H+i:i)})});7(o.17&&c.11)c.11(6(e,d){8 d>0?m(4-o.k):m(4+o.k)});7(o.J)1p(6(){m(4+o.k)},o.J+o.L);6 M(){8 f.D(4).D(0,v)};6 m(a){7(!b){7(o.K)o.K.Z(G,M());7(o.u){7(a<=o.B-v-1){9.5(A,-((l-(v*2))*g)+"C");4=a==o.B-v-1?l-(v*2)-1:l-(v*2)-o.k}F 7(a>=l-v+1){9.5(A,-((v)*g)+"C");4=a==l-v+1?v+1:v+o.k}F 4=a}F{7(a<0||a>l-v)8;F 4=a}b=12;9.1o(A=="w"?{w:-(4*g)}:{15:-(4*g)},o.L,o.16,6(){7(o.I)o.I.Z(G,M());b=q});7(!o.u){$(o.r+","+o.x).1n("X");$((4-o.k<0&&o.r)||(4+o.k>l-v&&o.x)||[]).1m("X")}}8 q}})};6 5(a,b){8 1l($.5(a[0],b))||0};6 s(a){8 a[0].1k+5(a,\'1j\')+5(a,\'1i\')};6 t(a){8 a[0].1t+5(a,\'1u\')+5(a,\'1e\')}})(1x);',62,96,'||||curr|css|function|if|return|ul|||||||||||scroll|itemLength|go|null||var|false|btnPrev|width|height|circular||left|btnNext|vertical||animCss|start|px|slice|tLi|else|this|visible|afterEnd|auto|beforeStart|speed|vis|btnGo|click|sizeCss|position|each|none|hidden|overflow|clone|tl|disabled|size|call|li|mousewheel|true|relative|index|top|easing|mouseWheel|padding|margin|200|float|visibility|append|marginBottom|extend|fn|prepend|marginRight|marginLeft|offsetWidth|parseInt|addClass|removeClass|animate|setInterval|0px|type|style|offsetHeight|marginTop|list|jCarouselLite|jQuery'.split('|'),0,{}))
+(function ($) {
+    $.fn.jCarouselLite = function (o) {
+        o = $.extend({
+            btnPrev: null,
+            btnNext: null,
+            btnGo: null,
+            mouseWheel: false,
+            auto: null,
+            speed: 200,
+            easing: null,
+            vertical: false,
+            circular: true,
+            visible: 3,
+            start: 0,
+            scroll: 1,
+            beforeStart: null,
+            afterEnd: null
+        }, o || {});
+        return this.each(function () {
+            var b = false,
+                animCss = o.vertical ? "top" : "left",
+                sizeCss = o.vertical ? "height" : "width";
+            var c = $(this),
+                ul = $("ul", c),
+                tLi = $("li", ul),
+                tl = tLi.size(),
+                v = o.visible;
+            if (o.circular) {
+                ul.prepend(tLi.slice(tl - v - 1 + 1).clone()).append(tLi.slice(0, v).clone());
+                o.start += v
+            }
+            var f = $("li", ul),
+                itemLength = f.size(),
+                curr = o.start;
+            c.css("visibility", "visible");
+            f.css({
+                overflow: "hidden",
+                float: o.vertical ? "none" : "left"
+            });
+            ul.css({
+                margin: "0",
+                padding: "0",
+                position: "relative",
+                "list-style-type": "none",
+                "z-index": "1"
+            });
+            c.css({
+                overflow: "hidden",
+                position: "relative",
+                "z-index": "2",
+                left: "0px"
+            });
+            var g = o.vertical ? height(f) : width(f);
+            var h = g * itemLength;
+            var j = g * v;
+            f.css({
+                width: f.width(),
+                height: f.height()
+            });
+            ul.css(sizeCss, h + "px").css(animCss, -(curr * g));
+            c.css(sizeCss, j + "px");
+            if (o.btnPrev) $(o.btnPrev).click(function () {
+                    return go(curr - o.scroll)
+                });
+            if (o.btnNext) $(o.btnNext).click(function () {
+                    return go(curr + o.scroll)
+                });
+            if (o.btnGo) $.each(o.btnGo, function (i, a) {
+                    $(a).click(function () {
+                        return go(o.circular ? o.visible + i : i)
+                    })
+                });
+            if (o.mouseWheel && c.mousewheel) c.mousewheel(function (e, d) {
+                    return d > 0 ? go(curr - o.scroll) : go(curr + o.scroll)
+                });
+            if (o.auto) setInterval(function () {
+                    go(curr + o.scroll)
+                }, o.auto + o.speed);
+
+            function vis() {
+                return f.slice(curr).slice(0, v)
+            };
+
+            function go(a) {
+                if (!b) {
+                    if (o.beforeStart) o.beforeStart.call(this, vis());
+                    if (o.circular) {
+                        if (a <= o.start - v - 1) {
+                            ul.css(animCss, -((itemLength - (v * 2)) * g) + "px");
+                            curr = a == o.start - v - 1 ? itemLength - (v * 2) - 1 : itemLength - (v * 2) - o.scroll
+                        } else if (a >= itemLength - v + 1) {
+                            ul.css(animCss, -((v) * g) + "px");
+                            curr = a == itemLength - v + 1 ? v + 1 : v + o.scroll
+                        } else curr = a
+                    } else {
+                        if (a < 0 || a > itemLength - v) return;
+                        else curr = a
+                    }
+                    b = true;
+                    ul.animate(animCss == "left" ? {
+                        left: -(curr * g)
+                    } : {
+                        top: -(curr * g)
+                    }, o.speed, o.easing, function () {
+                        if (o.afterEnd) o.afterEnd.call(this, vis());
+                        b = false
+                    });
+                    if (!o.circular) {
+                        $(o.btnPrev + "," + o.btnNext).removeClass("disabled");
+                        $((curr - o.scroll < 0 && o.btnPrev) || (curr + o.scroll > itemLength - v && o.btnNext) || []).addClass("disabled")
+                    }
+                }
+                return false
+            }
+        })
+    };
+
+    function css(a, b) {
+        return parseInt($.css(a[0], b)) || 0
+    };
+
+    function width(a) {
+        return a[0].offsetWidth + css(a, 'marginLeft') + css(a, 'marginRight')
+    };
+
+    function height(a) {
+        return a[0].offsetHeight + css(a, 'marginTop') + css(a, 'marginBottom')
+    }
+})(jQuery);
